9feacef318c7b5d9eef1b49ac82f625b17ab562e
unknown <mcsalazar122@gmail.com>
2022-08-27 23:01:04 -0500

    models complete

--- models/thought.js
+++ models/thought.js
@@ -1,18 +1,21 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('..utils/dateFormat');
 
 const ThoughtSchema = new Schema({
-thoughtText: {
+thoughtText: {//required: 1-280 char
     type: String,
-    //required: 1-280 char
+    min: [1],
+    max: [280, 'no need to write a book, let trim this down a bit']  
 },
 createdAt: {
-    type: Date.now,
-    //getter method to format timestamp o query 
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
 },
-userName:
+userName://required
 {
-    type: String
-    //required
+    type: String,
+    required: true
 },
 reactions: {
     type: array
--- models/user.js
+++ models/user.js
@@ -1,27 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     userName: {
-        type: String
-        //unique, required, trimmed
+      //unique, required, trimmed
+      type: String,
+      unique: true,
+      required: true,
     },
     email: {
-        type: String
-        //required, unique,validate
-
+      //required, unique,validate
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: () => Promise.resolve(false),
+        message: "enter valid email",
+      },
     },
-    thoughts: {
-        type: Object
-        //aray of _id references Thought model
+    thoughts: [//aray of _id references Thought model
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [//ary of _id values referencing User model
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
     },
-    friends: {
-        type: Object
-        //are of _id values referencing User model
-    }
-});
+    id: false,
+  }
+);
 
-
-
-const User = model('User', UserSchema);
+const User = model("User", UserSchema);
 
 module.exports = { User, Thought };
